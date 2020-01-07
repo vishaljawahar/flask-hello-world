c28bffb951b79993be21c1bddda4f1d0024691bf
Vishal Jawahar Muddumallesh <vishaljawahar@Vishals-MacBook-Pro.local>
2020-01-07 21:06:31 +0530

    Changes to server code

--- app.py
+++ app.py
@@ -6,15 +6,34 @@ from flask import Flask
 # create the application object
 app = Flask(__name__)
 
+# error handling
+app.config["DEBUG"] = True
+
+
 # use the decorator pattern to
 # link the view function to a url
-
-
 @app.route("/")
 @app.route("/hello")
 # define the view using a function, which returns a string
 def hello_world():
-    return "Hello World!"
+    return "Hello World!!!!"
+
+# dynamic route
+
+
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
+# dynamic route with explicit status code
+
+
+@app.route("/name/<name>")
+def index(name):
+    if name.lower == "michael":
+        return "Hello, {}".format(name)
+    else:
+        return "Not Found", 404
 
 
 # start the development server using run() method
